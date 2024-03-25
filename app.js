e398f4e71418b83cf5e654da93d8df8d0caaea31
Hamzabaloch08 <hb.developer08@gmail.com>
2024-03-25 07:57:40 +0500

    TABLE GENERATER

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var userInput = +prompt("Which table you want");
 var userInput2 = +prompt("1 TILL ?");
 
-
 for (var i = 1; i <= userInput2; i++) {
   document.write(userInput + " x " + i + " = " + userInput * i + "<br>");
 }
