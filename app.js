c6e358e69f33738dbdd10f53d5b7d57bf27cb03e
Hamzabaloch08 <hb.developer08@gmail.com>
2024-03-25 07:56:23 +0500

    TABLE GENERATER

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var userInput = +prompt("Which table you want");
 var userInput2 = +prompt("1 TILL ?");
 
+
 for (var i = 1; i <= userInput2; i++) {
   document.write(userInput + " x " + i + " = " + userInput * i + "<br>");
 }
+
+// My first table generater in JS create with for loop  and prompts from users to enter the number of the table they want and thay want 1 till 10 ,20 ,30 ,100
